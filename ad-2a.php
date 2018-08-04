945ca321d693bad8f31ba876b87544e60d42114f
Romain Torrente <romain.torrente@bde-insa-lyon.fr>
2018-08-04 11:41:47 +0200

    Page pour les AD 2A

---
+++ ad-2a.php
@@ -0,0 +1,194 @@
+<!DOCTYPE HTML>
+<!--
+	Binary by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-120533460-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-120533460-1');
+		</script>
+		<title>Astérix By CdpRecords</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="css/style-global.css" />
+
+		<script src="./js/jquery/jquery-1.10.2.js"></script>
+		<script src="./js/page-mot.js"></script>
+
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<a href="/" class="logo"><strong>Intégration 2018</strong> INSA Lyon</a>
+				<div class="navigation">
+					<span class=""><a href="index.php">Introduction</a></span>
+					<i class="fas fa-angle-right"></i>
+					<span class="actual"><a href="mot.php">Mot aux parents</a></span>
+					<i class="fas fa-angle-right"></i>
+					<span class=""><a href="carte.php">Découverte du village</a></span>
+					<i class="fas fa-angle-right"></i>
+					<span class=""><a href="todo.php">To-Do-List</a></span>
+				</div>
+				<div>
+					<ul id="blocToDoBtn" class="actions">
+						<li><a href="todo.php" class="button alt scrolly">Voir la to-do-list</a></li>
+					</ul>
+				</div>
+			</header>
+
+		<!-- Nav
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="generic.html">Generic</a></li>
+					<li><a href="elements.html">Elements</a></li>
+				</ul>
+			</nav> -->
+
+
+
+
+
+
+		<!-- Main -->
+			<section id="main">
+				<div class="inner">
+					<!--<div class="image fit">
+						<img src="images/pic11.jpg" alt="" />
+					</div>-->
+
+					<img id="motParent_Debut" src="content/img/motParent_Debut.svg"></img>
+
+					<header class="presParent" >
+						<h1 >Admis direct 2A</h1>
+						<p class="info" id="motParentsApercus">Si tu es sur cette page c’est que tu as été admis à l’INSA Lyon en 2 ème Année ! Bravo
+pour ton admission. Comme tu peux le constater le site de l’intégration est
+principalement consacré aux primo-entrant à l’INSA. Mais rassure-toi, nous ne
+t’avons pas oublié, tu va pouvoir participer aussi à l’intégration et passer une
+semaine de folie.</p>
+					</header>
+				<div class="titre">Ta situation</div>
+
+<p>Tu es ce qu’on appelle un Admis Direct en 2 ème année, plus communément appelé
+« AD 2A ». Tu vas donc vivre l’intégration aux coté des « parrains ». Ce sont les 2 ème s
+années qui accueillent leurs bizuth (les 1 ère années entrants). Dans ton cas tu n’auras
+pas de bizuth mais bien un parrain 2A qui t’accompagnera dans ton intégration.
+Tu fais ainsi partie de la 61ème promo et tu es un Mario (le thème d’inté de la promo
+61). Tu peux d’ores et déjà rejoindre le groupe Facebook :
+« <a href="https://www.facebook.com/groups/1556771174354091/" target="_blank">INSA Lyon – Admis en 1 ère année 2017/2018 – 61 ème Promo</a> »</p>
+
+<div class="titre">Ta situation</div>
+<p>Maintenant que tu connais ta situation, je t’invite à surfer sur le site de l’intégration.
+C’est sur ce site que tu apprendras tout sur ta future vie à l’INSA Lyon. Lorsque tu lis
+les sections, garde à l’esprits que tu es en 2A</p>
+
+<img id="motParent_Idefix" src="content/img/motParent_Idefix.svg"></img>
+
+<div class="titre">Les sections qui ne te concernent pas :</div>
+<p><i>Ton déguisement :</i><br/>Le thème de l’intégration cette année c’est Astérix et Obélix. Ce sont les 1 ère années
+qui se déguisent pour qu’on les reconnaissent tous. Toi tu es en 2A tu n’as donc pas
+de déguisement à préparer. Tu pourras admirer avec tes copains 2A les magnifiques
+oreilles d’Astérix que les bizuth porteront fièrement.</p>
+<img id="motParent_Velo" src="content/img/motParent_Velo.svg"></img>
+<p><i>Les Turnes :</i><br/>Ne prends pas peur en lisant la section des turnes car OUI tu vas échapper aux
+Bâtiment A et B (tu peux sauter de joie si tu veux). Tu seras directement avec les
+2èmes années de ta promo au bâtiment C &amp; D qui sont plus modernes, plus spacieux,
+et qui disposent de toilettes dans la chambre.</p>
+
+
+	
+	
+<div class="titre">Le Questionnaire spécial 2A :</div>
+<p>Pour que nous puissions t’attribuer un parrain tu dois, au même titre que les bizuth,
+remplir un questionnaire de parrainage. Cela permettra à tes amis CdP de t’attribuer
+dans une famille qui te correspond.</p>
+
+<header>
+						<h1 class="titreTodo">La TO-DO-LIST d’un AD :</h1>
+					</header>
+
+
+					<div class="todoBloc">
+						<div class="titre">Pendant cet été</div>
+
+						<div class="blocList">
+
+							<div class="partListe"><i id="joinFb" class="fab fa-facebook-square"></i>Rejoindre le groupe Facebook "INSA Lyon – Admis en 1ère année 2017/2018 - 61ème promo" <a href="https://www.facebook.com/groups/1556771174354091/" target="_blank"><button class="decouvrir">Rejoindre</button></a></div>
+
+							<div class="partListe"><i id="downloadIcon" class="fas fa-cloud-download-alt"></i>Télécharger le questionnaire AD 2A<a href="questionnaire_ad_2A.pdf" target="_blank"><button class="decouvrir">Télécharger le questionnaire</button></a></div>
+
+							<div class="partListe"><i id="printerIcon" class="fas fa-print"></i>Imprimer et envoyer le questionnaire concocté par tes CDP chéris, à cette adresse(hyper important, sinon tu n'auras pas de parrain!) :  </div> 
+
+							<div class="adresse">INSA, Bureau des élèves, Equipe CdP <br>
+
+							Le Thélème - Maison des Etudiants <br>
+
+							20, Avenue Albert Einstein <br>
+
+							69621 Villeurbanne CEDEX </div>
+
+							<div class="partListe"><i id="sunIcon" class="fas fa-sun"></i>Si tu veux participer au WEI <a href="wei.php"><button class="decouvrir">Inscription au Wei</button></a></div>
+						 </div>
+
+						
+
+						
+
+						 
+
+						<div class="titre">A la rentrée</div>  
+
+						<div class="blocList">
+							<div class="partListe"><i id="adminIcon" class="fas fa-clipboard-list"></i>Chaîne administrative:  </div>  
+
+								<div class="ssElemen">Confirmer ton inscription à la Vie Associative (parce que la VA c'est le feu) </div>  
+
+							<div class="partListe"><i id="heartIcon" class="fas fa-heart"></i>Passer au stand CdP (pour avoir des câlins) et surtout :  </div>  
+
+								<div class="ssElemen">Demander le nom de ton parrain </div>  
+
+							<div class="partListe"><i id="planeIcon" class="fas fa-globe"></i>Passer au stand Co-WEI pour : </div>  
+
+								<div class="ssElemen">Confirmer ton inscription </div>  
+
+								<div class="ssElemen">Choisir ton bus </div>  
+
+							<div class="partListe"><i id="shopIcon" class="fas fa-shopping-bag"></i> Passer au stand INSA Shop si tu veux utiliser ton coupon </div>  
+						</div>
+					</div>
+
+	
+					
+				</div>
+
+				
+			</section> 
+
+		
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="copyright">
+					<!--&copy; Untitled. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com">Unsplash</a>.-->
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
