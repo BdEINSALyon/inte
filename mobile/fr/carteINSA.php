b025778defd0a94fdc74d330a63d1aec8cb922aa
Mathéo Atché <matheo.atche@bde-insa-lyon.fr>
2019-07-10 14:05:29 +0200

    ajout de google analytics sur les pages restantes

--- mobile/fr/carteINSA.php
+++ mobile/fr/carteINSA.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-143625507-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', 'UA-143625507-1');
+        </script>
+
 		<title>Inté 2019 - BDE INSA Lyon</title>
 		<link rel="icon" type="image/png" href="../../images/global/cdp_logo.png" />
 		
